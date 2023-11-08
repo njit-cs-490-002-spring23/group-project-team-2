f8340d2a21a40435636765a21db408592201c680
Jordan Bruce <jeb79@njit.edu>
2023-11-07 21:41:48 -0500

    added common error constants that may occur in game.

--- townService/src/lib/InvalidParametersError.ts
+++ townService/src/lib/InvalidParametersError.ts
@@ -1,3 +1,19 @@
+/**
+ * constant variables to represent common game messages
+ */
+export const GAME_FULL_MESSAGE = 'Game is full';
+export const GAME_NOT_IN_PROGRESS_MESSAGE = 'Game is not in progress';
+export const GAME_OVER_MESSAGE = 'Game is over';
+export const GAME_ID_MISSMATCH_MESSAGE = 'Game ID mismatch';
+/**
+ * invalid move messages
+ */
+export const MOVE_NOT_YOUR_TURN_MESSAGE = 'Not your turn';
+/**
+ * invalid player messages
+ */
+export const PLAYER_NOT_IN_GAME_MESSAGE = 'Player is not in this game';
+export const PLAYER_ALREADY_IN_GAME_MESSAGE = 'Player is already in this game';
 export default class InvalidParametersError extends Error {
   public message: string;
 
---
+++ townService/src/town/games/MafiaGame.ts
@@ -0,0 +1,9 @@
+import InvalidParametersError, {
+    BOARD_POSITION_NOT_EMPTY_MESSAGE,
+    GAME_FULL_MESSAGE,
+    GAME_NOT_IN_PROGRESS_MESSAGE,
+    MOVE_NOT_YOUR_TURN_MESSAGE,
+    PLAYER_ALREADY_IN_GAME_MESSAGE,
+    PLAYER_NOT_IN_GAME_MESSAGE,
+  } from '../../lib/InvalidParametersError';
+  

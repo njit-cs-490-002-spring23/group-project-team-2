2844037d259dadc41eb70febe66764ac49f49b62
= <ar72@njit.edu>
2023-11-10 13:27:59 -0500

    Rewrote test for 7,8,9,10 players to check for id along with the correct count just for villagers and mafia.

--- townService/src/town/PlayerJoin.test.ts
+++ townService/src/town/PlayerJoin.test.ts
@@ -53,5 +53,129 @@ describe('Mafia', () => {
         expect(validRole(mafia, players)).toEqual(2);
       });
     });
+    describe('7 players', () => {
+      beforeEach(() => {
+        player1 = createPlayerForTesting();
+        player2 = createPlayerForTesting();
+        player3 = createPlayerForTesting();
+        player4 = createPlayerForTesting();
+        player5 = createPlayerForTesting();
+        player6 = createPlayerForTesting();
+        player7 = createPlayerForTesting();
+        game.join(player1);
+        game.join(player2);
+        game.join(player3);
+        game.join(player4);
+        game.join(player5);
+        game.join(player6);
+        game.join(player7);
+      });
+      it('only 3 villagers are present', () => {
+        const { villagers } = game.state.villagers;
+        const players = [player1, player2, player3, player4, player5, player6];
+        expect(validRole(villagers, players)).toEqual(3);
+      });
+      it('only 2 mafia are present', () => {
+        const { mafia } = game.state.mafia;
+        const players = [player1, player2, player3, player4, player5, player6];
+        expect(validRole(mafia, players)).toEqual(2);
+      });
+    });
+    describe('8 players', () => {
+      beforeEach(() => {
+        player1 = createPlayerForTesting();
+        player2 = createPlayerForTesting();
+        player3 = createPlayerForTesting();
+        player4 = createPlayerForTesting();
+        player5 = createPlayerForTesting();
+        player6 = createPlayerForTesting();
+        player7 = createPlayerForTesting();
+        player8 = createPlayerForTesting();
+        game.join(player1);
+        game.join(player2);
+        game.join(player3);
+        game.join(player4);
+        game.join(player5);
+        game.join(player6);
+        game.join(player7);
+        game.join(player8);
+      });
+      it('only 3 villagers are present', () => {
+        const { villagers } = game.state.villagers;
+        const players = [player1, player2, player3, player4, player5, player6];
+        expect(validRole(villagers, players)).toEqual(3);
+      });
+      it('only 3 mafia are present', () => {
+        const { mafia } = game.state.mafia;
+        const players = [player1, player2, player3, player4, player5, player6];
+        expect(validRole(mafia, players)).toEqual(3);
+      });
+    });
+    describe('9 players', () => {
+      beforeEach(() => {
+        player1 = createPlayerForTesting();
+        player2 = createPlayerForTesting();
+        player3 = createPlayerForTesting();
+        player4 = createPlayerForTesting();
+        player5 = createPlayerForTesting();
+        player6 = createPlayerForTesting();
+        player7 = createPlayerForTesting();
+        player8 = createPlayerForTesting();
+        player9 = createPlayerForTesting();
+        game.join(player1);
+        game.join(player2);
+        game.join(player3);
+        game.join(player4);
+        game.join(player5);
+        game.join(player6);
+        game.join(player7);
+        game.join(player8);
+        game.join(player9);
+      });
+      it('only 4 villagers are present', () => {
+        const { villagers } = game.state.villagers;
+        const players = [player1, player2, player3, player4, player5, player6];
+        expect(validRole(villagers, players)).toEqual(4);
+      });
+      it('only 3 mafia are present', () => {
+        const { mafia } = game.state.mafia;
+        const players = [player1, player2, player3, player4, player5, player6];
+        expect(validRole(mafia, players)).toEqual(3);
+      });
+    });
+    describe('10 players', () => {
+      beforeEach(() => {
+        player1 = createPlayerForTesting();
+        player2 = createPlayerForTesting();
+        player3 = createPlayerForTesting();
+        player4 = createPlayerForTesting();
+        player5 = createPlayerForTesting();
+        player6 = createPlayerForTesting();
+        player7 = createPlayerForTesting();
+        player8 = createPlayerForTesting();
+        player9 = createPlayerForTesting();
+        player10 = createPlayerForTesting();
+        game.join(player1);
+        game.join(player2);
+        game.join(player3);
+        game.join(player4);
+        game.join(player5);
+        game.join(player6);
+        game.join(player7);
+        game.join(player8);
+        game.join(player9);
+        game.join(player10);
+      });
+      it('only 5 villagers are present', () => {
+        const { villagers } = game.state.villagers;
+        const players = [player1, player2, player3, player4, player5, player6];
+        expect(validRole(villagers, players)).toEqual(5);
+      });
+      it('only 3 mafia are present', () => {
+        const { mafia } = game.state.mafia;
+        const players = [player1, player2, player3, player4, player5, player6];
+        expect(validRole(mafia, players)).toEqual(3);
+      });
+    });
   });
 });

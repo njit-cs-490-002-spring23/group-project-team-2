8dcbf5ab71e7e5249a5e3320ccf9f86b741febfb
Arnoldo Rivera <ar72@njit.edu>
2023-11-27 09:08:46 -0500

    Added two more test for leaving.

--- townService/src/town/PlayerJoin.test.ts
+++ townService/src/town/PlayerJoin.test.ts
@@ -1,6 +1,11 @@
 import { createPlayerForTesting, isIdInArray } from '../TestUtils';
 import Player from '../lib/Player';
 import MafiaGame from '../game/MafiaGame';
+import {
+  PLAYER_ALREADY_IN_GAME_MESSAGE,
+  GAME_FULL_MESSAGE,
+  PLAYER_NOT_IN_GAME_MESSAGE,
+} from '../lib/InvalidParametersError';
 
 const validRole = (role: string[], players: Player[]) => {
   let roleCount = 0;
@@ -12,7 +17,7 @@ const validRole = (role: string[], players: Player[]) => {
   return roleCount;
 };
 
-describe('Mafia', () => {
+describe('Mafia Game', () => {
   let game: MafiaGame;
   beforeEach(() => {
     game = new MafiaGame();
@@ -286,6 +291,83 @@ describe('Mafia', () => {
           }
         }
         expect(checkPolice).toBe(1);
+      });
+    });
+    it('should throw an error if the players is already in the game', () => {
+      player1 = createPlayerForTesting();
+      game.join(player1);
+      expect(() => game.join(player1)).toThrowError(PLAYER_ALREADY_IN_GAME_MESSAGE);
+      player2 = createPlayerForTesting();
+    });
+    it('should throw an error if the game is full', () => {
+      player1 = createPlayerForTesting();
+      player2 = createPlayerForTesting();
+      player3 = createPlayerForTesting();
+      player4 = createPlayerForTesting();
+      player5 = createPlayerForTesting();
+      player6 = createPlayerForTesting();
+      player7 = createPlayerForTesting();
+      player8 = createPlayerForTesting();
+      player9 = createPlayerForTesting();
+      player10 = createPlayerForTesting();
+      game.join(player1);
+      game.join(player2);
+      game.join(player3);
+      game.join(player4);
+      game.join(player5);
+      game.join(player6);
+      game.join(player7);
+      game.join(player8);
+      game.join(player9);
+      game.join(player10);
+
+      const player11 = createPlayerForTesting();
+      expect(() => game.join(player11)).toThrowError(GAME_FULL_MESSAGE);
+    });
+  });
+  describe('Leaving', () => {
+    it('should throw an error if the player is not in the game', () => {
+      const player = createPlayerForTesting();
+      game.join(player);
+      expect(() => game.leave(player)).toThrowError(PLAYER_NOT_IN_GAME_MESSAGE);
+    });
+    it('when the game is not in progress, the game status should be set to WAITING_TO_START and remove the player if there is less than 6 players', () => {
+      const player1 = createPlayerForTesting();
+      const player2 = createPlayerForTesting();
+      const player3 = createPlayerForTesting();
+      const player4 = createPlayerForTesting();
+      const player5 = createPlayerForTesting();
+      game.join(player1);
+      game.join(player2);
+      game.join(player3);
+      game.join(player4);
+      game.join(player5);
+      game.leave(player5);
+      expect(game.state.status).toBe('WAITING_TO_START');
+    });
+    describe('when players are in the game', () => {
+      beforeEach(() => {
+        const player1 = createPlayerForTesting();
+        const player2 = createPlayerForTesting();
+        const player3 = createPlayerForTesting();
+        const player4 = createPlayerForTesting();
+        const player5 = createPlayerForTesting();
+        const player6 = createPlayerForTesting();
+        game.join(player1);
+        game.join(player2);
+        game.join(player3);
+        game.join(player4);
+        game.join(player5);
+        game.join(player6);
+      });
+      it('if all mafia memebers leave the game while the game is in progress the villagers should win', () => {
+        expect(game.state).toBe('IN_PROGRESS');
+        const mafiaPlayer1 = game.state.mafia[0];
+        const mafiaPlayer2 = game.state.mafia[1];
+        game.leave(mafiaPlayer1);
+        game.leave(mafiaPlayer2);
+        expect(game.state).toBe('OVER');
+        expect(game.state.status.winnerTeam).toBe('CIVILIANS_TEAM');
       });
     });
   });

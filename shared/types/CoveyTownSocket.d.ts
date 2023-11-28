5ca396bb1e1faf337fc7a9b67a15c8842232223b
Jordan Bruce <jeb79@njit.edu>
2023-11-28 13:20:03 -0500

    edited leave method. 1 lint error remains from the applyMove method

---
+++ shared/types/CoveyTownSocket.d.ts
@@ -0,0 +1,244 @@
+import { type } from "os";
+
+export type TownJoinResponse = {
+  /** Unique ID that represents this player * */
+  userID: string;
+  /** Secret token that this player should use to authenticate
+   * in future requests to this service * */
+  sessionToken: string;
+  /** Secret token that this player should use to authenticate
+   * in future requests to the video service * */
+  providerVideoToken: string;
+  /** List of players currently in this town * */
+  currentPlayers: Player[];
+  /** Friendly name of this town * */
+  friendlyName: string;
+  /** Is this a private town? * */
+  isPubliclyListed: boolean;
+  /** Current state of interactables in this town */
+  interactables: Interactable[];
+}
+
+export type Interactable = ViewingArea | ConversationArea;
+
+export type TownSettingsUpdate = {
+  friendlyName?: string;
+  isPubliclyListed?: boolean;
+}
+
+export type Direction = 'front' | 'back' | 'left' | 'right';
+export type PlayerID = string;
+export interface Player {
+  id: PlayerID;
+  userName: string;
+  location: PlayerLocation;
+};
+
+export type XY = { x: number, y: number };
+
+export interface PlayerLocation {
+  /* The CENTER x coordinate of this player's location */
+  x: number;
+  /* The CENTER y coordinate of this player's location */
+  y: number;
+  /** @enum {string} */
+  rotation: Direction;
+  moving: boolean;
+  interactableID?: string;
+};
+export type ChatMessage = {
+  author: string;
+  sid: string;
+  body: string;
+  dateCreated: Date;
+};
+
+export interface ConversationArea {
+  id: string;
+  topic?: string;
+  occupantsByID: string[];
+};
+export interface BoundingBox {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
+export interface ViewingArea {
+  id: string;
+  video?: string;
+  isPlaying: boolean;
+  elapsedTimeSec: number;
+}
+
+export interface ServerToClientEvents {
+  playerMoved: (movedPlayer: Player) => void;
+  playerDisconnect: (disconnectedPlayer: Player) => void;
+  playerJoined: (newPlayer: Player) => void;
+  initialize: (initialData: TownJoinResponse) => void;
+  townSettingsUpdated: (update: TownSettingsUpdate) => void;
+  townClosing: () => void;
+  chatMessage: (message: ChatMessage) => void;
+  interactableUpdate: (interactable: Interactable) => void;
+}
+
+export interface ClientToServerEvents {
+  chatMessage: (message: ChatMessage) => void;
+  playerMovement: (movementData: PlayerLocation) => void;
+  interactableUpdate: (update: Interactable) => void;
+}
+
+/* Game */
+export type GameStatus = 'IN_PROGRESS' | 'WAITING_TO_START' | 'OVER';
+/**
+ * Base type for the state of a game
+ */
+export interface GameState {
+  status: GameStatus;
+}
+
+export type Teams = 'CIVILIANS_TEAM' | 'MAFIAS_TEAM';
+/**
+ * Type for the state of a game that can be won
+ */
+export interface WinnableGameState extends GameState {
+  winners?: PlayerID[];
+  winnerTeam?: Teams;
+}
+
+/**
+ * Base type for a move in a game. Implementers should also extend MoveType
+ * @see MoveType
+ */
+export interface GameMove<MoveType> {
+  playerID: PlayerID;
+  gameID: GameInstanceID;
+  move: MoveType;
+}
+
+/**
+ * Type for a move in Mafia Game
+ */
+export interface VoteMove {
+  playerVoting: PlayerID;
+  playerVoted: PlayerID;
+}
+
+/**
+ * The current player status of a current Mafia game player
+ */
+export type PlayerStatus = 'Alive' | 'Deceased';
+
+/**
+ * type to define the time of day: day cycle or night cycle
+ */
+export type TimeOfDay = 'Day' | 'Night';
+
+/**
+ * Represents the state of a current Mafia game player.
+ */
+export interface PlayerState {
+  id: PlayerID;
+  status: PlayerStatus;
+}
+
+/**
+ * Type for the state of a Mafia game
+ * The state of the game is represented as a list of moves, and the playerIDs of the players (2 villagers, 2 mafia, one police, one doctor)
+ * When player join, they will be assign to the roles randonly
+ */
+export interface MafiaGameState extends WinnableGameState {
+  moves: ReadonlyArray<VoteMove>;
+  villagers?: PlayerState[];
+  mafias?: PlayerState[];
+  police?: PlayerState;
+  doctor?: PlayerState;
+  phase?: TimeOfDay;
+}
+
+export type InteractableID = string;
+export type GameInstanceID = string;
+
+/**
+ * Type for the result of a game
+ */
+export interface GameResult {
+  gameID: GameInstanceID;
+  scores: { [playerName: string]: number };
+}
+
+/**
+ * Base type for an *instance* of a game. An instance of a game
+ * consists of the present state of the game (which can change over time),
+ * the players in the game, and the result of the game
+ * @see GameState
+ */
+export interface GameInstance<T extends GameState> {
+  state: T;
+  id: GameInstanceID;
+  players: PlayerID[];
+  result?: GameResult;
+}
+
+/**
+ * Base type for an area that can host a game
+ * @see GameInstance
+ */
+export interface GameArea<T extends GameState> extends Interactable {
+  game: GameInstance<T> | undefined;
+  history: GameResult[];
+}
+
+export type CommandID = string;
+
+/**
+ * Base type for a command that can be sent to an interactable.
+ * This type is used only by the client/server interface, which decorates
+ * an @see InteractableCommand with a commandID and interactableID
+ */
+interface InteractableCommandBase {
+  /**
+   * A unique ID for this command. This ID is used to match a command against a response
+   */
+  commandID: CommandID;
+  /**
+   * The ID of the interactable that this command is being sent to
+   */
+  interactableID: InteractableID;
+  /**
+   * The type of this command
+   */
+  type: string;
+}
+
+export type InteractableCommand =  ViewingAreaUpdateCommand | JoinGameCommand | GameMoveCommand<TicTacToeMove> | LeaveGameCommand;
+export interface ViewingAreaUpdateCommand  {
+  type: 'ViewingAreaUpdate';
+  update: ViewingArea;
+}
+export interface JoinGameCommand {
+  type: 'JoinGame';
+}
+export interface LeaveGameCommand {
+  type: 'LeaveGame';
+  gameID: GameInstanceID;
+}
+export interface GameMoveCommand<MoveType> {
+  type: 'GameMove';
+  gameID: GameInstanceID;
+  move: MoveType;
+}
+export type InteractableCommandReturnType<CommandType extends InteractableCommand> = 
+  CommandType extends JoinGameCommand ? { gameID: string}:
+  CommandType extends ViewingAreaUpdateCommand ? undefined :
+  CommandType extends GameMoveCommand<TicTacToeMove> ? undefined :
+  CommandType extends LeaveGameCommand ? undefined :
+  never;
+
+export type InteractableCommandResponse<MessageType> = {
+  commandID: CommandID;
+  interactableID: InteractableID;
+  error?: string;
+  payload?: InteractableCommandResponseMap[MessageType];
+}
---
+++ townService/src/town/games/MafiaGame.ts
@@ -0,0 +1,234 @@
+import Game from './Game';
+import { GameMove, MafiaGameState, PlayerID, VoteMove } from '../../types/CoveyTownSocket';
+import Player from '../../lib/Player';
+import InvalidParametersError, {
+  GAME_FULL_MESSAGE,
+  GAME_NOT_IN_PROGRESS_MESSAGE,
+  PLAYER_ALREADY_IN_GAME_MESSAGE,
+  PLAYER_NOT_IN_GAME_MESSAGE,
+} from '../../lib/InvalidParametersError';
+
+/**
+ * A helper function to roll a dice.
+ * The maximum is inclusive and the minimum is inclusive.
+ */
+function getRandomIntInclusive(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+/**
+ * A MafiaGame is a Game that implements the rules of Mafia.
+ */
+export default class MafiaGame extends Game<MafiaGameState, VoteMove> {
+  public constructor() {
+    super({
+      moves: [],
+      status: 'WAITING_TO_START',
+    });
+  }
+
+  /**
+   * A helper function to randomly assign a player to one of the Roles in the Mafia Game.
+   * @param player The player that will be assigned a role in the game
+   * @returns nothing. Only used for role assignment and updating the game state to reflect the role assignment.
+   */
+  private _randomlyAssignRoleTo(player: Player): void {
+    let diceRoll: number;
+    let roleAssigned = false;
+    let minDiceNumber = 1;
+    let maxDiceNumber = 4;
+    if (this._players.length < 6) {
+      while (!roleAssigned) {
+        diceRoll = getRandomIntInclusive(minDiceNumber, maxDiceNumber);
+        if (diceRoll === 1 && !this.state.police) {
+          this.state.police = {
+            id: player.id,
+            status: 'Alive',
+          };
+          roleAssigned = true;
+        }
+        if (diceRoll === 2 && !this.state.doctor) {
+          this.state.doctor = {
+            id: player.id,
+            status: 'Alive',
+          };
+          roleAssigned = true;
+        }
+        if (diceRoll === 3 && this.state.villagers) {
+          if (!this.state.villagers[0]) {
+            this.state.villagers[0] = {
+              id: player.id,
+              status: 'Alive',
+            };
+            roleAssigned = true;
+          }
+          if (!this.state.villagers[1]) {
+            this.state.villagers[1] = {
+              id: player.id,
+              status: 'Alive',
+            };
+            roleAssigned = true;
+          }
+        }
+        if (diceRoll === 4 && this.state.mafias) {
+          if (!this.state.mafias[0]) {
+            this.state.mafias[0] = {
+              id: player.id,
+              status: 'Alive',
+            };
+            roleAssigned = true;
+          }
+          if (!this.state.mafias[1]) {
+            this.state.mafias[1] = {
+              id: player.id,
+              status: 'Alive',
+            };
+            roleAssigned = true;
+          }
+        }
+      }
+    } else {
+      minDiceNumber = 1;
+      maxDiceNumber = 2;
+      while (!roleAssigned) {
+        diceRoll = getRandomIntInclusive(minDiceNumber, maxDiceNumber);
+        if (diceRoll === 1 && this.state.villagers) {
+          if (!this.state.villagers[2]) {
+            this.state.villagers[2] = {
+              id: player.id,
+              status: 'Alive',
+            };
+            roleAssigned = true;
+          }
+          if (!this.state.villagers[3]) {
+            this.state.villagers[3] = {
+              id: player.id,
+              status: 'Alive',
+            };
+            roleAssigned = true;
+          }
+        }
+        if (diceRoll === 2 && this.state.mafias) {
+          if (!this.state.mafias[2]) {
+            this.state.mafias[2] = {
+              id: player.id,
+              status: 'Alive',
+            };
+            roleAssigned = true;
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * A helper function that iterates over the array of current Mafia team members
+   *  and checks if all Mafia members are 'Deceased'
+   * @returns boolean value
+   */
+  private _allMafiaIsDead(): boolean {
+    let playerIndex: number;
+    if (this.state.mafias) {
+      for (playerIndex = 0; playerIndex < this.state.mafias?.length; playerIndex += 1) {
+        if (this.state.mafias[playerIndex]?.status === 'Alive') {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  /**
+   * A helper function that iterates over array of current game players
+   * @call to _allMafiaIsDead() helper method
+   * If all mafia team members player status is 'Deceased',
+   * @returns an array of all winning civilian PlayerID and doctor PlayerID and police PlayerID
+   * else:
+   * @returns an array of all mafia members PlayerID
+   */
+  private _getWinningTeam(): PlayerID[] {
+    let playerIndex: number;
+    const winningTeam: PlayerID[] = [];
+    if (this._allMafiaIsDead() && this.state.doctor && this.state.police && this.state.villagers) {
+      winningTeam.push(this.state.doctor?.id);
+      winningTeam.push(this.state.police?.id);
+      for (playerIndex = 0; playerIndex < this.state.villagers?.length; playerIndex += 1) {
+        winningTeam.push(this.state.villagers[playerIndex].id);
+      }
+      return winningTeam;
+    }
+    if (this.state.mafias) {
+      for (playerIndex = 0; playerIndex < this.state.mafias.length; playerIndex += 1) {
+        winningTeam.push(this.state.mafias[playerIndex].id);
+      }
+    }
+    return winningTeam;
+  }
+
+  /**
+   * Applies a player's move to the game. In this game a move would be a vote.
+   */
+  public applyMove(move: GameMove<VoteMove>): void {
+    if (this.state.status !== 'IN_PROGRESS') {
+      throw new InvalidParametersError(GAME_NOT_IN_PROGRESS_MESSAGE);
+    }
+  }
+
+  /**
+   * Adds a player to the game
+   * Updates the game's state to reflect the new player.
+   * Once the first player joins the game, updates the game's status to 'WAITING_TO_START'.
+   * If the game is now full (has 10 players), updates the game's state to set the status to IN_PROGRESS.
+   * @param player The player to join the game.
+   * @throws InvalidParametersError if the player is already in the game (PLAYER_ALREADY_IN_GAME_MESSAGE)
+   * or the game is full (GAME_FULL_MESSAGE)
+   */
+  public _join(player: Player): void {
+    if (this._players.includes(player)) {
+      throw new InvalidParametersError(PLAYER_ALREADY_IN_GAME_MESSAGE);
+    }
+    if (this._players.length === 0) {
+      this._randomlyAssignRoleTo(player);
+      this.state.status = 'WAITING_TO_START';
+    }
+    if (this._players.length < 6) {
+      this._randomlyAssignRoleTo(player);
+    }
+    if (this._players.length === 10) {
+      throw new InvalidParametersError(GAME_FULL_MESSAGE);
+    }
+  }
+
+  /**
+   * Removes a player from the game.
+   * Updates the game's state to reflect the player leaving.
+   * If the game has at least 6 players in it at the time of call to this method,
+   *   updates the game's status to OVER and sets the winner to the other team.
+   * @param player The player to remove from the game
+   * @throws InvalidParametersError if the player is not in the game (PLAYER_NOT_IN_GAME_MESSAGE)
+   */
+  public _leave(player: Player): void {
+    let currentGameStatus = this.state.status;
+    const minNumberOfPlayers = 6;
+    const maxNumberOfPlayers = 10;
+    if (!this._players.includes(player)) {
+      throw new InvalidParametersError(PLAYER_NOT_IN_GAME_MESSAGE);
+    }
+    if (this._players.length === minNumberOfPlayers && currentGameStatus === 'IN_PROGRESS') {
+      currentGameStatus = 'OVER';
+      this.state.winners = this._getWinningTeam();
+    }
+    if (this._players.length === maxNumberOfPlayers && currentGameStatus === 'IN_PROGRESS') {
+      currentGameStatus = 'OVER';
+      this.state.winners = this._getWinningTeam();
+    }
+    if (this.state.status === 'OVER' && this.state.winners) {
+      if (this._allMafiaIsDead()) {
+        this.state.winnerTeam = 'CIVILIANS_TEAM';
+      } else {
+        this.state.winnerTeam = 'MAFIAS_TEAM';
+      }
+    }
+  }
+}
